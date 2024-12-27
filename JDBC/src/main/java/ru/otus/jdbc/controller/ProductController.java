36df239b17c5340ec002714807ace8b6792e884e
Edalik <www.ayakubov@yandex.ru>
2024-12-27 20:53:06 +0300

    ДЗ: Практика Spring Data JDBC. Правки

--- JDBC/src/main/java/ru/otus/jdbc/controller/ProductController.java
+++ JDBC/src/main/java/ru/otus/jdbc/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.jdbc.dto.ProductDTO;
+import ru.otus.jdbc.mapper.ProductMapper;
 import ru.otus.jdbc.service.ProductService;
 
 import java.util.List;
@@ -21,24 +22,27 @@ public class ProductController {
 
     private final ProductService productService;
 
+    private final ProductMapper productMapper;
+
     @PostMapping
     public ProductDTO upsertProduct(@RequestBody ProductDTO dto) {
-        return productService.upsertProduct(dto);
+        return productMapper.entityToDTO(productService.upsertProduct(dto));
     }
 
     @GetMapping
     public List<ProductDTO> getAllProducts() {
-        return productService.getAllProducts();
+        return productMapper.entityToDTO(productService.getAllProducts());
     }
 
     @GetMapping("/{id}")
     public ProductDTO getProductById(@PathVariable Long id) {
-        return productService.getProductById(id);
+        return productMapper.entityToDTO(productService.getProductById(id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProductById(@PathVariable Long id) {
-        return productService.deleteProductById(id);
+        productService.deleteProductById(id);
+        return ResponseEntity.status(204).build();
     }
 
 }
--- JDBC/src/main/java/ru/otus/jdbc/service/ProductService.java
+++ JDBC/src/main/java/ru/otus/jdbc/service/ProductService.java
@@ -2,7 +2,6 @@ package ru.otus.jdbc.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.otus.jdbc.dto.ProductDTO;
@@ -20,27 +19,23 @@ public class ProductService {
 
     private final ProductMapper productMapper;
 
-    public ProductDTO upsertProduct(ProductDTO dto) {
+    public Product upsertProduct(ProductDTO dto) {
         Product product = productMapper.DTOtoEntity(dto);
 
-        return productMapper.entityToDTO(productRepository.save(product));
+        return productRepository.save(product);
     }
 
-    public List<ProductDTO> getAllProducts() {
-        return productMapper.entityToDTO(productRepository.findAll());
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 
-    public ProductDTO getProductById(Long id) {
-        Product product = productRepository.findById(id)
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatusCode.valueOf(404)));
-
-        return productMapper.entityToDTO(product);
     }
 
-    public ResponseEntity<?> deleteProductById(Long id) {
+    public void deleteProductById(Long id) {
         productRepository.deleteById(id);
-
-        return ResponseEntity.status(204).build();
     }
 
 }
